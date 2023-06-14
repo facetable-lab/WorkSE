22096f5cf73a6bf78a168c9b1e2603f433fbdb77
facetable-lab <diofff6@gmail.com>
2023-06-15 02:50:52 +0300

    Регистрация модели ЯП в админке

--- src/parser/admin.py
+++ src/parser/admin.py
@@ -11,3 +11,13 @@ class CityAdmin(admin.ModelAdmin):
 
 
 admin.site.register(models.City, CityAdmin)
+
+
+class LanguageAdmin(admin.ModelAdmin):
+    """Отображение и функционал модели Language в админке"""
+
+    # Генерация слага
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(models.Language, LanguageAdmin)
