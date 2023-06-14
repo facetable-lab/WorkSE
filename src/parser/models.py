1d36cc81bfb9d3554f1a3b1b2a31276285ff9c31
facetable-lab <diofff6@gmail.com>
2023-06-15 02:04:39 +0300

    Модель населенных пунктов

--- src/parser/models.py
+++ src/parser/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    """Модель населенных пунктов"""
+    name = models.CharField('Город', max_length=50)
+    slug = models.CharField(max_length=60, blank=True)
+
+    def __str__(self):
+        """Строковое представление объекта, в т.ч и в админке"""
+        return self.name
+
+    class Meta:
+        """Отображение названия модели в админке"""
+
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
