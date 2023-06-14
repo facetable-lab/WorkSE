f6326c763d372845018a339a24a933b6d44eccb6
facetable-lab <diofff6@gmail.com>
2023-06-15 02:03:57 +0300

    Русификация

--- src/parser/apps.py
+++ src/parser/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ParserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'parser'
+
+    # Отображение названия приложения в админке
+    verbose_name = 'Поисковая система'
