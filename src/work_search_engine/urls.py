1f8f39b460e731f92744063895eff3dc21855238
facetable-lab <diofff6@gmail.com>
2023-06-15 18:02:32 +0300

    Другой контроллер

--- src/work_search_engine/urls.py
+++ src/work_search_engine/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from parser import views
+from parser.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view),
+    path('', home_view),
 ]
