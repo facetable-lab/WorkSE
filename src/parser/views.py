ae3687da3e516547decaa6d288c4f9beac0afc2c
facetable-lab <diofff6@gmail.com>
2023-06-15 18:01:43 +0300

    Передача данных в шаблон

--- src/parser/views.py
+++ src/parser/views.py
@@ -5,7 +5,7 @@ from .models import Vacancy
 
 def home_view(_request):
     _context = {
-        'object_list': Vacancy.objects.all(),
+        'vacancy_qs': Vacancy.objects.all(),
     }
 
-    return render(request=_request, template_name='home.html', context=_context)
+    return render(request=_request, template_name='parser/home.html', context=_context)
