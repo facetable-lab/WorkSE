9e1d69f655948fbf425467fb8c201eeb87244bfd
facetable-lab <diofff6@gmail.com>
2023-06-18 17:09:48 +0300

    Передача формы

--- src/parser/views.py
+++ src/parser/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 
 from .models import Vacancy
+from .forms import FindForm
 
 
 def home_view(_request):
+    city = _request.GET.get('city')
+    language = _request.GET.get('language')
+
+    _filter = {}
+    if city or language:
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+
     _context = {
-        'vacancy_qs': Vacancy.objects.all(),
+        'form': FindForm,
+        'vacancy_qs': Vacancy.objects.filter(**_filter),
     }
 
     return render(request=_request, template_name='parser/home.html', context=_context)
