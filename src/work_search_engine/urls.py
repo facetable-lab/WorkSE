8f28130e6d24e36dab0c773f23da0fe5f41f3427
facetable-lab <diofff6@gmail.com>
2023-06-18 16:04:39 +0300

    Вынес маршруты приложения в папку с приложением

--- src/work_search_engine/urls.py
+++ src/work_search_engine/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-
-from parser.views import home_view
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view),
+    path('', include('parser.urls')),
 ]
