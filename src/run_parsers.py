8f9f74f994a2c8533d1f66e8e0b366c2a879676c
facetable-lab <diofff6@gmail.com>
2023-06-20 01:50:37 +0300

    Запуск django вне проекта

--- src/run_parsers.py
+++ src/run_parsers.py
@@ -1,9 +1,22 @@
+import os, sys
+
+
+project = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(project)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'work_search_engine.settings'
+
+import django
+django.setup()
+
+from parser.models import Vacancy, City, Language
 from parser.parsers import *
 
 _parsers = (
     (head_hunter, 'https://rostov.hh.ru/search/vacancy?text=python&salary=&area=76&ored_clusters=true'),
     (habr_career, 'https://career.habr.com/vacancies?locations[]=c_726&q=python&type=all'),
 )
+
+city = City.objects.filter(slug='rostov-na-donu')
 
 jobs, errors = [], []
 
@@ -17,4 +30,3 @@ for i in jobs:
 
 for i in errors:
     print(i)
-
