1071ad44e367540175d2d9681e5a358cb421a1c7
facetable-lab <diofff6@gmail.com>
2023-06-20 02:54:13 +0300

    Написал модель Url для хранения пар город/яп

--- src/parser/models.py
+++ src/parser/models.py
@@ -1,4 +1,13 @@
+import json
+
 from django.db import models
+
+
+def default_urls():
+    return {
+        'head_hunter': '',
+        'habr_career': ''
+    }
 
 
 class City(models.Model):
@@ -18,6 +27,7 @@ class City(models.Model):
 
 
 class Language(models.Model):
+    """Модель языков программирования"""
     name = models.CharField('Язык программирования', max_length=50)
     slug = models.CharField(max_length=60, blank=True, verbose_name='URL', unique=True)
 
@@ -31,6 +41,7 @@ class Language(models.Model):
 
 
 class Vacancy(models.Model):
+    """Модель населенных вакансий"""
     url = models.URLField()
     title = models.CharField('Должность', max_length=250)
     company = models.CharField('Компания', max_length=250)
@@ -51,8 +62,9 @@ class Vacancy(models.Model):
 
 
 class Error(models.Model):
-    timestamp = models.DateField(auto_now_add=True)
-    data = models.JSONField(default={},)
+    """Модель ошибок"""
+    timestamp = models.DateField(auto_now_add=True, verbose_name='Время')
+    data = models.JSONField(default={}, verbose_name='Данные')
 
     def __str__(self):
         return str(self.timestamp)
@@ -60,3 +72,26 @@ class Error(models.Model):
     class Meta:
         verbose_name = 'Ошибка'
         verbose_name_plural = 'Ошибки'
+
+
+class PrettyJSONEncoder(json.JSONEncoder):
+    """Выравнивание внещнего вида данных в поле Json модели Url"""
+
+    def __init__(self, *args, indent, sort_keys, **kwargs):
+        super().__init__(*args, indent=4, sort_keys=True, **kwargs)
+
+
+class Url(models.Model):
+    """Модель ссылок на сайты-первоисточники"""
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, verbose_name='Язык программирования')
+    url_data = models.JSONField(default=default_urls, verbose_name='Маршруты', encoder=PrettyJSONEncoder)
+
+    def __str__(self):
+        return f'{self.city} | {self.language}'
+
+    class Meta:
+        unique_together = ('city', 'language')
+
+        verbose_name = 'Ссылка'
+        verbose_name_plural = 'Ссылки'
